33698f1ab4e65ca6f3671c3f7664a7bc0c6c9ba8
Mikel Fernandes <mikelfernandes404@gmail.com>
2025-07-11 22:15:36 +0530

    mongo Db connect

---
+++ src/index.js
@@ -0,0 +1,49 @@
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import express from "express";
+
+dotenv.config({
+    path: './.env'
+})
+
+const app = express();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+// import mongoose from "mongoose"
+// import {DB_NAME} from "./constants.js"
+// import express from "express"
+
+// const app = express();
+
+// ;( async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.error("Error",error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is Listening on PORT: ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("Error",error)
+//         throw error
+//     }
+// }) ()
