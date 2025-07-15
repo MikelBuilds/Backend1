96ede1b515b8cc7a7e9ded5a926f452be5534442
Mikel Fernandes <mikelfernandes404@gmail.com>
2025-07-15 22:00:17 +0530

    made routes

--- src/app.js
+++ src/app.js
@@ -10,9 +10,14 @@ app.use(cors({
 }));
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extendend:true,limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRoute from './routes/user.routes.js'
+
+
+app.use("/app/v1/users",userRoute)
 
 export {app};
 
